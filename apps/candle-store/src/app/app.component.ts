d9f9012f6daf975b6701082980d0334496c200bf
Joshua Calafell <joshcalafell@gmail.com>
2024-02-05 16:17:27 -0800

    Footer

--- apps/candle-store/src/app/app.component.ts
+++ apps/candle-store/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { HeaderComponent } from 'libs/ui-organisms/src'
+import { FooterComponent, HeaderComponent } from 'libs/ui-organisms/src'
 
 @Component({
 	standalone: true,
-	imports: [HeaderComponent, RouterModule],
+	imports: [HeaderComponent, RouterModule, FooterComponent],
 	selector: 'rfs-dev-atomic-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
