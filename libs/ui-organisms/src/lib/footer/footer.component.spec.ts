78545f1ccef02460432ff4ea045e183a7238143a
Joshua Calafell <joshcalafell@gmail.com>
2024-02-05 16:32:32 -0800

    Unit tests

--- libs/ui-organisms/src/lib/footer/footer.component.spec.ts
+++ libs/ui-organisms/src/lib/footer/footer.component.spec.ts
@@ -1,21 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FooterComponent } from './footer.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { FooterComponent } from './footer.component'
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+	let component: FooterComponent
+	let fixture: ComponentFixture<FooterComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FooterComponent],
-    }).compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [FooterComponent],
+		}).compileComponents()
 
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(FooterComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	})
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+	it('should create', () => {
+		expect(component).toBeTruthy()
+	})
+
+	it('should render the footer', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('.my-footer')).toBeTruthy()
+	})
+})
+
+// Path: libs/ui-organisms/src/lib/footer/footer.component.ts
