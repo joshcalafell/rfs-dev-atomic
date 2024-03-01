0e51159cab1a973ac6b8b0240d86a482b3862e83
Joshua Calafell <joshcalafell@gmail.com>
2024-03-01 01:14:05 -0800

    Button group WIP

--- libs/atomic/molecules/ui-button-row/src/lib/ui-button-row/ui-button-row.component.stories.ts
+++ libs/atomic/molecules/ui-button-row/src/lib/ui-button-row/ui-button-row.component.stories.ts
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/angular'
 import { UiButtonRowComponent } from './ui-button-row.component'
 
-import { within } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
+import { within } from '@storybook/testing-library'
 
 const meta: Meta<UiButtonRowComponent> = {
 	component: UiButtonRowComponent,
@@ -12,13 +12,116 @@ export default meta
 type Story = StoryObj<UiButtonRowComponent>
 
 export const Primary: Story = {
-	args: {},
+	args: {
+		buttons: [
+			{
+				label: 'Button 1',
+				color: 'primary',
+				size: 'medium',
+				disabled: false,
+			},
+			{
+				label: 'Button 2',
+				color: 'secondary',
+				size: 'medium',
+				disabled: false,
+			},
+			{
+				label: 'Button 3',
+				color: 'tertiary',
+				size: 'medium',
+				disabled: false,
+			},
+		],
+	},
 }
 
-export const Heading: Story = {
-	args: {},
+export const Small: Story = {
+	args: {
+		buttons: [
+			{
+				label: 'Button 1',
+				color: 'primary',
+				size: 'small',
+				disabled: false,
+			},
+			{
+				label: 'Button 2',
+				color: 'secondary',
+				size: 'small',
+				disabled: false,
+			},
+			{
+				label: 'Button 2',
+				color: 'tertiary',
+				size: 'small',
+				disabled: false,
+			},
+		],
+	},
 	play: async ({ canvasElement }) => {
 		const canvas = within(canvasElement)
-		expect(canvas.getByText(/ui-button-row works!/gi)).toBeTruthy()
+		expect(canvas.getByText(/Button 7/gi)).toBeTruthy()
+		expect(document.querySelector('.ui-button')).toBeTruthy()
+	},
+}
+
+export const Medium: Story = {
+	args: {
+		buttons: [
+			{
+				label: 'Button 4',
+				color: 'primary',
+				size: 'medium',
+				disabled: false,
+			},
+			{
+				label: 'Button 5',
+				color: 'secondary',
+				size: 'medium',
+				disabled: false,
+			},
+			{
+				label: 'Button 6',
+				color: 'tertiary',
+				size: 'medium',
+				disabled: false,
+			},
+		],
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement)
+		expect(canvas.getByText(/Button 6/gi)).toBeTruthy()
+		expect(document.querySelector('.ui-button')).toBeTruthy()
+	},
+}
+
+export const Large: Story = {
+	args: {
+		buttons: [
+			{
+				label: 'Button 7',
+				color: 'primary',
+				size: 'large',
+				disabled: false,
+			},
+			{
+				label: 'Button 8',
+				color: 'secondary',
+				size: 'large',
+				disabled: false,
+			},
+			{
+				label: 'Button 9',
+				color: 'tertiary',
+				size: 'large',
+				disabled: false,
+			},
+		],
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement)
+		expect(canvas.getByText(/Button 9/gi)).toBeTruthy()
+		expect(document.querySelector('.ui-button')).toBeTruthy()
 	},
 }
